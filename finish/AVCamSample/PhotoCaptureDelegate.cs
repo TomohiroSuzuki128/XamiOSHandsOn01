2653aaeb496e036dde267d5fcba8a2fa5a81a4b7
Tomohiro Suzuki <suzuki@frog-pod.com>
2018-09-22 16:29:46 +0900

    コードのiOS12対応終了

--- finish/AVCamSample/PhotoCaptureDelegate.cs
+++ finish/AVCamSample/PhotoCaptureDelegate.cs
@@ -57,15 +57,15 @@ namespace AVCamSample
 			willCapturePhotoAnimation();
 		}
 
-		public override void DidFinishProcessingPhoto(AVCapturePhotoOutput captureOutput, CMSampleBuffer photoSampleBuffer, CMSampleBuffer previewPhotoSampleBuffer, AVCaptureResolvedPhotoSettings resolvedSettings, AVCaptureBracketedStillImageSettings bracketSettings, NSError error)
-		{
-			if (photoSampleBuffer != null)
-				photoData = AVCapturePhotoOutput.GetJpegPhotoDataRepresentation(photoSampleBuffer, previewPhotoSampleBuffer);
-			else
-				Console.WriteLine($"Error capturing photo: {error.LocalizedDescription}");
-		}
+        public override void DidFinishProcessingPhoto(AVCapturePhotoOutput output, AVCapturePhoto photo, NSError error)
+        {
+            if (error != null)
+                Console.WriteLine($"Error capturing photo: {error.LocalizedDescription}");
+            else
+                photoData = photo.FileDataRepresentation;
+        }
 
-		public override void DidFinishRecordingLivePhotoMovie(AVCapturePhotoOutput captureOutput, NSUrl outputFileUrl, AVCaptureResolvedPhotoSettings resolvedSettings)
+        public override void DidFinishRecordingLivePhotoMovie(AVCapturePhotoOutput captureOutput, NSUrl outputFileUrl, AVCaptureResolvedPhotoSettings resolvedSettings)
 		{
 			capturingLivePhoto(false);
 		}
@@ -80,30 +80,130 @@ namespace AVCamSample
 			livePhotoCompanionMovieUrl = outputFileUrl;
 		}
 
-		public override void DidFinishCapture(AVCapturePhotoOutput captureOutput, AVCaptureResolvedPhotoSettings resolvedSettings, NSError error)
+
+
+
+        // func photoOutput(_ output: AVCapturePhotoOutput, didFinishCaptureFor resolvedSettings: AVCaptureResolvedPhotoSettings, error: Error?) {
+        //     if let error = error {
+        //         print("Error capturing photo: \(error)")
+        //         didFinish()
+        //         return
+        //     }
+
+        //     guard let photoData = photoData else {
+        //         print("No photo data resource")
+        //         didFinish()
+        //         return
+        //     }
+
+        //     PHPhotoLibrary.requestAuthorization { [unowned self] status in
+        //         if status == .authorized {
+        //             PHPhotoLibrary.shared().performChanges({ [unowned self] in
+        //                 let options = PHAssetResourceCreationOptions()
+        //                 let creationRequest = PHAssetCreationRequest.forAsset()
+        //                 options.uniformTypeIdentifier = self.requestedPhotoSettings.processedFileType.map { $0.rawValue }
+        //                 creationRequest.addResource(with: .photo, data: photoData, options: options)
+
+        //                 if let livePhotoCompanionMovieURL = self.livePhotoCompanionMovieURL {
+        //                     let livePhotoCompanionMovieFileResourceOptions = PHAssetResourceCreationOptions()
+        //                     livePhotoCompanionMovieFileResourceOptions.shouldMoveFile = true
+        //                     creationRequest.addResource(with: .pairedVideo, fileURL: livePhotoCompanionMovieURL, options: livePhotoCompanionMovieFileResourceOptions)
+        //                 }
+
+        //                 }, completionHandler: { [unowned self] _, error in
+        //                     if let error = error {
+        //                         print("Error occurered while saving photo to photo library: \(error)")
+        //                     }
+
+        //                     self.didFinish()
+        //                 }
+        //             )
+        //         } else {
+        //             self.didFinish()
+        //         }
+        //     }
+        // }
+
+
+
+
+        public override void DidFinishCapture(AVCapturePhotoOutput captureOutput, AVCaptureResolvedPhotoSettings resolvedSettings, NSError error)
 		{
-			if (error != null)
+            //     if let error = error {
+            //         print("Error capturing photo: \(error)")
+            //         didFinish()
+            //         return
+            //     }
+            if (error != null)
 			{
 				Console.WriteLine($"Error capturing photo: {error.LocalizedDescription})");
 				DidFinish();
 				return;
 			}
 
-			if (photoData == null)
+            //     guard let photoData = photoData else {
+            //         print("No photo data resource")
+            //         didFinish()
+            //         return
+            //     }
+            if (photoData == null)
 			{
 				Console.WriteLine("No photo data resource");
 				DidFinish();
 				return;
 			}
 
-			PHPhotoLibrary.RequestAuthorization(status => {
-				if (status == PHAuthorizationStatus.Authorized)
-				{
-					PHPhotoLibrary.SharedPhotoLibrary.PerformChanges(() => {
-						var creationRequest = PHAssetCreationRequest.CreationRequestForAsset();
-						creationRequest.AddResource(PHAssetResourceType.Photo, photoData, null);
 
-						var url = livePhotoCompanionMovieUrl;
+            //     PHPhotoLibrary.requestAuthorization { [unowned self] status in
+            //         if status == .authorized {
+            //             PHPhotoLibrary.shared().performChanges({ [unowned self] in
+            //                 let options = PHAssetResourceCreationOptions()
+            //                 let creationRequest = PHAssetCreationRequest.forAsset()
+            //                 options.uniformTypeIdentifier = self.requestedPhotoSettings.processedFileType.map { $0.rawValue }
+            //                 creationRequest.addResource(with: .photo, data: photoData, options: options)
+
+            //                 if let livePhotoCompanionMovieURL = self.livePhotoCompanionMovieURL {
+            //                     let livePhotoCompanionMovieFileResourceOptions = PHAssetResourceCreationOptions()
+            //                     livePhotoCompanionMovieFileResourceOptions.shouldMoveFile = true
+            //                     creationRequest.addResource(with: .pairedVideo, fileURL: livePhotoCompanionMovieURL, options: livePhotoCompanionMovieFileResourceOptions)
+            //                 }
+
+            //                 }, completionHandler: { [unowned self] _, error in
+            //                     if let error = error {
+            //                         print("Error occurered while saving photo to photo library: \(error)")
+            //                     }
+
+            //                     self.didFinish()
+            //                 }
+            //             )
+            //         } else {
+            //             self.didFinish()
+            //         }
+            //     }
+
+            // PHPhotoLibrary.requestAuthorization { [unowned self] status in
+            PHPhotoLibrary.RequestAuthorization(status => {
+                // if status == .authorized {
+                if (status == PHAuthorizationStatus.Authorized)
+				{
+                    // PHPhotoLibrary.shared().performChanges({ [unowned self] in
+                    PHPhotoLibrary.SharedPhotoLibrary.PerformChanges(() => {
+                        // let options = PHAssetResourceCreationOptions()
+                        var options = new PHAssetResourceCreationOptions();
+                        // let creationRequest = PHAssetCreationRequest.forAsset()
+                        var creationRequest = PHAssetCreationRequest.CreationRequestForAsset();
+                        // options.uniformTypeIdentifier = self.requestedPhotoSettings.processedFileType.map { $0.rawValue }
+                        options.UniformTypeIdentifier = RequestedPhotoSettings.ProcessedFileType;
+                        // creationRequest.addResource(with: .photo, data: photoData, options: options)
+                        creationRequest.AddResource(PHAssetResourceType.Photo, photoData, options);
+
+                        // if let livePhotoCompanionMovieURL = self.livePhotoCompanionMovieURL {
+                        // let livePhotoCompanionMovieFileResourceOptions = PHAssetResourceCreationOptions()
+                        // livePhotoCompanionMovieFileResourceOptions.shouldMoveFile = true
+                        // creationRequest.addResource(with: .pairedVideo, fileURL: livePhotoCompanionMovieURL, options: livePhotoCompanionMovieFileResourceOptions)
+                        // }
+
+                        var url = livePhotoCompanionMovieUrl;
 						if (url != null)
 						{
 							var livePhotoCompanionMovieFileResourceOptions = new PHAssetResourceCreationOptions
@@ -125,5 +225,5 @@ namespace AVCamSample
 			});
 		}
 
-	}
+    }
 }
